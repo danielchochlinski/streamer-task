d1707da72c6b1ba9ad905480491dfdad1ccc9ebf
Daniel <danielchochlisnki@hotmail.com>
2023-06-24 19:32:06 +0200

    api test

--- src/__test__/app.test.ts
+++ src/__test__/app.test.ts
@@ -3,7 +3,7 @@ import { app } from '../server';
 
 describe('App', () => {
     test('should listen on the configured port', async () => {
-        const response = await request(app).get('/');
+        const response = await request(app).get('/api/');
         expect(response.statusCode).toBe(200);
         // Add more assertions to validate the response if needed
     });
