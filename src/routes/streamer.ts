3b30b0efa3aba495ac1816193838c1f3f8ac2d4e
Daniel <danielchochlisnki@hotmail.com>
2023-07-01 21:44:38 -0400

    -test

--- src/routes/streamer.ts
+++ src/routes/streamer.ts
@@ -1,10 +1,19 @@
 import { Router } from 'express';
 import { imageMiddleware } from '../middleware/imageMiddleware';
-import { createStreamer, findStreamerByName, getAllStreamers, voteForStreamer } from '../controllers/streamersController';
+import {
+    createStreamer,
+    findStreamerByName,
+    getAllStreamerNames,
+    getAllStreamers,
+    getPopularStreamers,
+    voteForStreamer
+} from '../controllers/streamersController';
 const router = Router();
 
+router.get('/streamers-names', getAllStreamerNames);
 router.get('/streamers', getAllStreamers);
 router.post('/streamer', imageMiddleware, createStreamer);
 router.put('/streamer/:id', voteForStreamer);
 router.get('/streamer', findStreamerByName);
+router.get('/streamers/popular', getPopularStreamers);
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,14 @@ import { connectToDatabase } from './config/db';
 import bodyParser from 'body-parser';
 import testRouter from './routes/test';
 import streamerRouter from './routes/streamer';
-
+import cors from 'cors';
 export const app = express();
 
 connectToDatabase();
+
 app.use(bodyParser.json());
+
+app.use(cors());
 
 app.use('/api', testRouter);
 app.use('/api', streamerRouter);
